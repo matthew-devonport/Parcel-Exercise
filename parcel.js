223be82bed7aa457bcb7309842fbabb356ed30c4
matthew-devonport <roarsintheeast@gmail.com>
2020-03-04 12:53:41 +1300

    added dependencies & .js files

---
+++ parcel.js
@@ -0,0 +1,89 @@
+const getShippingCost = (parcel) => {
+
+  
+if (parcel === null)  {
+   const noPackage = {
+       error: "No package provided"
+   }
+   return noPackage
+}
+
+if (parcel.length === null) {
+    const noLength = {
+        error: "No length provided"
+    }
+    return noLength
+}
+
+if (parcel.breadth === null) {
+    const noBreadth = {
+        error: "No breadth provided"
+    }
+    return noBreadth
+}
+
+if (parcel.height === null) {
+    const noHeight = {
+        error: "No height provided"
+    }
+    return noHeight
+}
+
+if (parcel.weight === null) {
+    const noWeight = {
+        error: "No weight provided"
+    }
+    return noWeight
+}
+
+    
+if (parcel.weight >= 25) {
+
+ const heavyPackage = {
+    error: "Too heavy!"
+    }
+    return heavyPackage
+}
+
+if (parcel.length <= 200 &&
+    parcel.breadth <= 300 &&
+    parcel.height <= 150) {
+
+    const smallPackage = {
+        name: "Small",
+        cost: 5
+    }
+    return smallPackage
+}
+
+if (parcel.length <= 300 &&
+    parcel.breadth <= 400 &&
+    parcel.height <= 200) {
+
+    const mediumPackage = {
+        name: "Medium",
+        cost: 7.5
+    }
+    return mediumPackage
+}
+
+if (parcel.length <= 400 &&
+    parcel.breadth <= 600 &&
+    parcel.height <= 250) {
+
+    const largePackage = {
+        name: "Large",
+        cost: 8.5
+    }
+    return largePackage
+} else  {
+    const tooLarge = {
+        error: "Too large!"
+    }
+    return tooLarge
+}
+}
+
+module.exports = {
+    getShippingCost
+}
---
+++ tests/parcel.test.js
@@ -0,0 +1,100 @@
+const {getShippingCost} = require('../parcel')
+
+const missingLengthPackageTest = {
+    length: null,
+    breadth: 250,
+    height: 120,
+    weight: 5
+}
+
+const missingBreadthPackageTest = {
+    length: 150,
+    breadth: null,
+    height: 120,
+    weight: 5
+}
+
+const missingHeightPackageTest = {
+    length: 150,
+    breadth: 250,
+    height: null,
+    weight: 5
+}
+
+const missingWeightPackageTest = {
+    length: 150,
+    breadth: 250,
+    height: 120,
+    weight: null
+}
+
+
+const smallPackageTest = {
+    length: 150,
+    breadth: 250,
+    height: 120,
+    weight: 5
+}
+
+const mediumPackageTest = {
+    length: 250,
+    breadth: 250,
+    height: 120,
+    weight: 5
+}
+
+const largePackageTest = {
+    length: 350,
+    breadth: 250,
+    height: 120,
+    weight: 5
+}
+
+const extraLargePackageTest = {
+    length: 450,
+    breadth: 250,
+    height: 120,
+    weight: 5
+}
+
+const heavyPackageTest = {
+    length: 150,
+    breadth: 250,
+    height: 120,
+    weight: 25
+}
+
+
+test('small package', () => {
+   expect(getShippingCost(smallPackageTest).name).toBe("Small")
+   expect(getShippingCost(smallPackageTest).cost).toBe(5)
+})
+
+test('medium package', () => {
+    expect(getShippingCost(mediumPackageTest).name).toBe("Medium")
+    expect(getShippingCost(mediumPackageTest).cost).toBe(7.5)
+})
+
+test('large package', () => {
+    expect(getShippingCost(largePackageTest).name).toBe("Large")
+    expect(getShippingCost(largePackageTest).cost).toBe(8.5)
+})
+
+test('package too heavy', () => {
+    expect(getShippingCost(heavyPackageTest).error).toBe("Too heavy!")
+})
+
+test('package too large', () => {
+    expect(getShippingCost(extraLargePackageTest).error).toBe("Too large!")
+})
+
+test('no package', () => {
+    expect(getShippingCost(null).error).toBe("No package provided")
+})
+
+test('missing package details', () => {
+    expect(getShippingCost(missingLengthPackageTest).error).toBe("No length provided")
+    expect(getShippingCost(missingBreadthPackageTest).error).toBe("No breadth provided")
+    expect(getShippingCost(missingHeightPackageTest).error).toBe("No height provided")
+    expect(getShippingCost(missingWeightPackageTest).error).toBe("No weight provided")
+})
