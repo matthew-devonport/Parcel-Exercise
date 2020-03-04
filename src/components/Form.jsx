df797b63dd7e7baeea9037fe4276ca52a7959953
matthew-devonport <roarsintheeast@gmail.com>
2020-03-04 14:38:14 +1300

    set up react up

---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Form() {
+  return (
+  
+    <div>Delete me</div>
+  );
+}
+
+export default Form;
+
+
+
+// State - value of the 4 fields
+
+// UI Form - 4 fields, submit button and div with JS
